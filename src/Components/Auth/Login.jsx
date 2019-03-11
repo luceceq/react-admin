a59865ccd4c19596bf8d5c8cf373120f8001b11a
lucenildo <dev.luce84@outlook.com>
2019-03-11 00:08:22 -0300

    root css property fixed

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { push } from 'connected-react-router';
+import {connect} from 'react-redux';
+import {push} from 'connected-react-router';
 
-import { withStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
@@ -11,8 +11,8 @@ import Button from '@material-ui/core/Button';
 import './Auth.css';
 
 const styles = theme => ({
-  root: theme.mixins.gutters({
-    paddingBottom: theme.spacing.unit * 4,
+  root: theme.mixins.gutters ({
+    paddingBottom: theme.spacing.unit * 2,
     marginTop: theme.spacing.unit * 4,
     marginBottom: theme.spacing.unit * 4,
   }),
@@ -21,7 +21,7 @@ const styles = theme => ({
     width: 300,
   },
   button: {
-    marginTop: theme.spacing.unit * 4
+    marginTop: theme.spacing.unit * 4,
   },
 });
 
@@ -29,31 +29,31 @@ class Login extends Component {
   state = {
     username: '',
     password: '',
-  }
+  };
 
-  componentDidMount() {
+  componentDidMount () {
     if (this.props.auth.user.signInUserSession) {
-      this.props.changePage('dashboard');
+      this.props.changePage ('dashboard');
     } else {
-      window.location = `${process.env.REACT_APP_LOGIN_URL}/login?response_type=token&client_id=${process.env.REACT_APP_LOGIN_CLIENT_ID}&redirect_uri=${window.location.origin}/`
+      window.location = `${process.env.REACT_APP_LOGIN_URL}/login?response_type=token&client_id=${process.env.REACT_APP_LOGIN_CLIENT_ID}&redirect_uri=${window.location.origin}/`;
     }
   }
 
   onChangeInputField = (key, value) => {
-    this.setState({
-      [key]: value
-    })
-  }
+    this.setState ({
+      [key]: value,
+    });
+  };
 
-  render() {
-    const { classes } = this.props;
+  render () {
+    const {classes} = this.props;
 
     return (
       <Grid
         container
-        alignItems='center'
-        direction='row'
-        justify='center'
+        alignItems="center"
+        direction="row"
+        justify="center"
         className="auth"
       >
         <Grid item>
@@ -81,13 +81,13 @@ Login.propTypes = {
 };
 
 const mapDispatchToProps = {
-  changePage: (path = null) => push(`/${path}`),
-}
+  changePage: (path = null) => push (`/${path}`),
+};
 
 const mapStateToProps = state => ({
   auth: state.auth,
 });
 
-const LoginWithStyle = withStyles(styles)(Login);
+const LoginWithStyle = withStyles (styles) (Login);
 
-export default connect(mapStateToProps, mapDispatchToProps)(LoginWithStyle);
+export default connect (mapStateToProps, mapDispatchToProps) (LoginWithStyle);
