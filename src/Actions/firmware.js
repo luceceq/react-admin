71b37d5cdf66b7ce5ef6c55e72d513e4150bac77
lucenildo <dev.luce84@outlook.com>
2019-08-05 06:10:31 -0300

    fix variables

--- src/Actions/firmware.js
+++ src/Actions/firmware.js
@@ -5,29 +5,29 @@ import {
   ADD_FIRMWARE_DONE,
   ADD_FIRMWARE_ERROR,
   ADD_FIRMWARE_IS_LOADING,
-  DELETE_FIRMWARE,
-} from '../Reducers/firmware';
+  DELETE_FIRMWARE
+} from "../Reducers/firmware";
 
 import Request from "../request";
 
 function _getList(items) {
   return {
     type: GET_FIRMWARE_LIST,
-    data: items,
-  }
+    data: items
+  };
 }
 
 function _errorOccurred(err) {
   return {
     type: GET_FIRMWARE_ERROR,
-    error: err,
-  }
+    error: err
+  };
 }
 
 function _isLoading() {
   return {
-    type: GET_FIRMWARE_IS_LOADING,
-  }
+    type: GET_FIRMWARE_IS_LOADING
+  };
 }
 
 export function getFirmwareListRequest() {
@@ -35,86 +35,110 @@ export function getFirmwareListRequest() {
     dispatch(_isLoading());
 
     try {
-      var { auth: { user: { signInUserSession: { accessToken } } } } = getState();
+      var {
+        auth: {
+          user: {
+            signInUserSession: { accessToken }
+          }
+        }
+      } = getState();
     } catch (error) {
       return Promise.reject();
     }
 
     return Request({
-      url: 'firmwares',
-      method: 'get',
-      token: accessToken,
-    }).then(r => {
-      dispatch(_getList(r.data));
-    }).catch(e => {
-      if (e) {
-        dispatch(_errorOccurred(e));
-      }
+      url: "firmware",
+      method: "get",
+      token: accessToken
     })
-  }
+      .then(r => {
+        dispatch(_getList(r.data));
+      })
+      .catch(e => {
+        if (e) {
+          dispatch(_errorOccurred(e));
+        }
+      });
+  };
 }
 
 function _add(data) {
   return {
     type: ADD_FIRMWARE_DONE,
-    data: data,
-  }
+    data: data
+  };
 }
 
 function _addErrorOccurred(err) {
   return {
     type: ADD_FIRMWARE_ERROR,
-    error: err.errors.name,
-  }
+    error: err.errors.name
+  };
 }
 
 function _addIsLoading() {
   return {
-    type: ADD_FIRMWARE_IS_LOADING,
-  }
+    type: ADD_FIRMWARE_IS_LOADING
+  };
 }
 
 export function addFirmwareRequest(data) {
   return (dispatch, getState) => {
     dispatch(_addIsLoading());
-    let { auth: { user: { signInUserSession: { accessToken } } } } = getState();
+    let {
+      auth: {
+        user: {
+          signInUserSession: { accessToken }
+        }
+      }
+    } = getState();
 
     return Request({
-      url: 'firmwares',
-      method: 'post',
+      url: "firmwares",
+      method: "post",
       token: accessToken,
-      data: data,
-    }).then(r => {
-      dispatch(_add(r.data));
-      return r.data;
-    }).catch(e => {
-      if (e.response) {
-        dispatch(_addErrorOccurred(e.response.data));
-      }
+      data: data
     })
-  }
+      .then(r => {
+        dispatch(_add(r.data));
+        return r.data;
+      })
+      .catch(e => {
+        if (e.response) {
+          dispatch(_addErrorOccurred(e.response.data));
+        }
+      });
+  };
 }
 
 function _delete(id) {
   return {
     type: DELETE_FIRMWARE,
-    id: id,
-  }
+    id: id
+  };
 }
 
 export function deleteFirmwareRequest(id) {
   return (dispatch, getState) => {
-    let { auth: { user: { signInUserSession: { accessToken } } } } = getState();
+    let {
+      auth: {
+        user: {
+          signInUserSession: { accessToken }
+        }
+      }
+    } = getState();
 
     return Request({
       url: `firmwares/${id}`,
-      method: 'delete',
-      token: accessToken,
-    }).then(response => {
-      dispatch(_delete(id));
-      return Promise.resolve(true);
-    }).catch(err => {
-      return Promise.reject(false);
+      method: "delete",
+      token: accessToken
     })
-  }
+      .then(response => {
+        dispatch(_delete(id));
+        return Promise.resolve(true);
+      })
+      .catch(err => {
+        return Promise.reject(false);
+      });
+  };
 }
