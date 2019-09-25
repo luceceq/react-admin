b29d9914c945b2e2bf4f0d02f2534a6844047d8c
luceceq <dev.luce84@outlook.com>
2019-09-25 07:00:40 -0300

    make code clean

--- src/Components/Firmware/index.js
+++ src/Components/Firmware/index.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
-import { connect } from 'react-redux';
-import { push } from 'connected-react-router';
-
-import { withStyles } from '@material-ui/core/styles';
-
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableFooter from '@material-ui/core/TableFooter';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Paper from '@material-ui/core/Paper';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import { connect } from "react-redux";
+import { push } from "connected-react-router";
+import { withStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableFooter from "@material-ui/core/TableFooter";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Paper from "@material-ui/core/Paper";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 
 import AddNew from "./addNew";
 import TableRowC from "./tableRowC";
@@ -25,42 +23,42 @@ import { getFirmwareListRequest } from "../../Actions/firmware";
 
 const styles = theme => ({
   root: {
-    width: '100%',
-    position: 'relative',
-    marginTop: theme.spacing.unit * 3,
+    width: "100%",
+    position: "relative",
+    marginTop: theme.spacing.unit * 3
   },
   table: {
-    minWidth: 500,
+    minWidth: 500
   },
   tableWrapper: {
-    overflowX: 'auto',
+    overflowX: "auto"
   },
   toolbarRoot: {
     // paddingRight: theme.spacing.unit,
   },
   spacer: {
-    flex: '1 1 100%',
+    flex: "1 1 100%"
   },
   actions: {
     color: theme.palette.text.secondary,
-    whiteSpace: 'nowrap'
+    whiteSpace: "nowrap"
   },
   title: {
-    flex: '0 0 auto',
+    flex: "0 0 auto"
   },
   button: {
     margin: theme.spacing.unit,
-    marginLeft: 0,
+    marginLeft: 0
   },
   typography: {
-    padding: theme.spacing.unit * 3,
+    padding: theme.spacing.unit * 3
   },
   progress: {
-    display: 'block',
+    display: "block",
     margin: theme.spacing.unit * 4,
-    marginRight: 'auto',
-    marginLeft: 'auto',
-  },
+    marginRight: "auto",
+    marginLeft: "auto"
+  }
 });
 
 class Firmware extends Component {
@@ -70,13 +68,13 @@ class Firmware extends Component {
     this.state = {
       page: 0,
       rowsPerPage: 25,
-      rowPerPageOptions: [50, 100],
+      rowPerPageOptions: [50, 100]
     };
   }
 
   componentDidMount() {
     this.props.getFirmwareList().then(() => {
-        this.getRowPerPageOptions();
+      this.getRowPerPageOptions();
     });
   }
 
@@ -94,22 +92,22 @@ class Firmware extends Component {
 
     if (dataLength > this.state.rowsPerPage) {
       if (dataLength >= this.state.rowPerPageOptions[0]) {
-        arrayNum.push(this.state.rowPerPageOptions[0])
+        arrayNum.push(this.state.rowPerPageOptions[0]);
       } else {
-        arrayNum.push(dataLength)
+        arrayNum.push(dataLength);
       }
     }
 
     if (dataLength > this.state.rowPerPageOptions[0]) {
       if (dataLength >= this.state.rowPerPageOptions[1]) {
-        arrayNum.push(this.state.rowPerPageOptions[1])
+        arrayNum.push(this.state.rowPerPageOptions[1]);
       } else {
-        arrayNum.push(dataLength)
+        arrayNum.push(dataLength);
       }
     }
 
     this.setState({ rowPerPageOptions: arrayNum });
-  }
+  };
 
   render() {
     const { classes, firmwareList, isLoadingFirmwareList } = this.props;
@@ -133,9 +131,11 @@ class Firmware extends Component {
           </Toolbar>
 
           <div className={classes.tableWrapper}>
-            {isLoadingFirmwareList && <CircularProgress className={classes.progress} />}
+            {isLoadingFirmwareList && (
+              <CircularProgress className={classes.progress} />
+            )}
 
-            {!isLoadingFirmwareList &&
+            {!isLoadingFirmwareList && (
               <Table className={classes.table}>
                 <TableHead>
                   <TableRow>
@@ -148,13 +148,8 @@ class Firmware extends Component {
                   {firmwareList
                     .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                     .map(n => {
-                    return (
-                      <TableRowC
-                        key={n.id}
-                        firmware={n}
-                      />
-                    );
-                  })}
+                      return <TableRowC key={n.id} firmware={n} />;
+                    })}
                 </TableBody>
 
                 <TableFooter>
@@ -170,7 +165,7 @@ class Firmware extends Component {
                   </TableRow>
                 </TableFooter>
               </Table>
-            }
+            )}
           </div>
         </Paper>
       </React.Fragment>
@@ -182,24 +177,27 @@ Firmware.propTypes = {
   classes: PropTypes.object.isRequired,
   auth: PropTypes.object,
   firmwareList: PropTypes.array,
-  isLoadingFirmwareList: PropTypes.bool,
+  isLoadingFirmwareList: PropTypes.bool
 };
 
 const mapStateToProps = state => ({
   auth: state.auth,
   firmwareList: state.firmware.list,
-  isLoadingFirmwareList: state.firmware.isLoading,
+  isLoadingFirmwareList: state.firmware.isLoading
 });
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators(
     {
       changePage: (path = null) => push(`/${path}`),
-      getFirmwareList: () => getFirmwareListRequest(),
+      getFirmwareList: () => getFirmwareListRequest()
     },
-    dispatch,
+    dispatch
   );
 
 const FirmwareWithStyle = withStyles(styles)(Firmware);
 
-export default connect(mapStateToProps, mapDispatchToProps)(FirmwareWithStyle);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(FirmwareWithStyle);
